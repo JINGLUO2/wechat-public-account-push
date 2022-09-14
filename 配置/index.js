8ddc6a5ae7ddd67bbc2aa33fb13809c6499601d3
JINGLUO2 <113501082+JINGLUO2@users.noreply.github.com>
2022-09-14 17:24:39 +0800

    Update and rename config/index.js to 配置/index.js

--- 配置/index.js
+++ 配置/index.js
@@ -23,36 +23,36 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "omilF6HWrFj5GY1AQ7lNFp_5r-6E",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "UpUmQC9CpeaZZAqQKwkN1WEn6k63qZEo8qPnRzzyWSU",
       // 所在省份
-      province: "广东",
+      province: "湖北",
       // 所在城市
-      city: "肇庆",
+      city: "襄阳",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-25',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "生日", "name": "老婆", "year": "1984", "date": "07-25"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2022", "date": "06-11"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "生日", "name": "老公", "year": "1995", "date": "08-24"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2022", "date": "05-04"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
+        {"keyword": "love_day", date: "2022-05-04"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-06-11"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
@@ -102,7 +102,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "piOy9ypZFAEOWvPKLGnWVcyBH4cS5-KK6xT92_NjeHs",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -110,7 +110,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "omilF6HWrFj5GY1AQ7lNFp_5r-6E",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -205,7 +205,7 @@ export const config = {
 
   // 好文节选的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: ""
+  LITERARY_PREFERENCE: "网络"
 
 
 }
